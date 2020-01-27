7804e8bb0d59ad0f7d4be9ee6af3c2495646ce64
000059316 <bloodemon22@gmail.com>
2020-01-27 10:06:48 +0100

    commit tigran

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,9 +15,10 @@ class HomeController extends AbstractController
     /**
      * @Route("connexion", name="home_index", methods={"GET", "POST"})
      * @param Request $request
+     * @param EntityManagerInterface $entityManager
      * @return Response
      */
-    public function create(Request $request): Response {
+    public function create(Request $request, EntityManagerInterface $entityManager): Response {
 
         $user = new User();
 
@@ -26,12 +28,17 @@ class HomeController extends AbstractController
         if($formUser->isSubmitted() && $formUser->isValid()){
             $this->addFlash("success", "Utilisateur crée !" );
 
+            $entityManager->persist($user);
+            $entityManager->flush();
+
             return $this->redirectToRoute("home_index" );
         }
 
 // Appel a la vue
         return $this->render('Home/connexion.html.twig', ['formUser'=> $formUser->createView()
         ]);
+
+
     }
 
 
