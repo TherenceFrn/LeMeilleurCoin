28ac0680285d1772982fc917bf3b3cb769e8c052
FERRON Therence <therence.ferron@orange.fr>
2020-01-27 11:58:42 +0100

    commit delannonce fonctionnel

--- src/Controller/DelannonceController.php
+++ src/Controller/DelannonceController.php
@@ -15,20 +15,30 @@ class DelannonceController extends AbstractController
 
     /**
      * @Route("delannonce", name="delannonce_all", methods={"GET"})
-     * @Route("delannonce/{id}", name="delannonce_index", methods={"GET"})
+     * @Route("delannonce/{id}", name="delannonce_index", requirements={"id": "\d+"}, methods={"GET"})
      * @param Request $request
      * @return Response
      */
-
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
 
-
         if(null !== $request->get('id'))    {
             //s'il y a un id :
-            $annonces = $entityManager->getRepository('App:Annonce')->findAll();
 
+            $annonce = $entityManager->getRepository('App:Annonce')->find($request->get('id'));
+
+            dump($annonce);
+
+            if($annonce == null){
+            }else{
+              $entityManager->remove($annonce);
+              $entityManager->flush();
+            }
+
+
+            $annonces = $entityManager->getRepository('App:Annonce')->findAll();
             return $this->render('Delannonce/delannonce.html.twig', ['mesannonces' => $annonces]);
+
 
         }else{
             //s'il n'y a pas d'id :
