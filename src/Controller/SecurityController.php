685f9de1fe9fafa8df8093199d07f8f49aef9c2e
000059316 <bloodemon22@gmail.com>
2020-02-04 14:45:06 +0100

    commit login/logout (redirection logout non foncionnel)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,63 +2,37 @@
 
 namespace App\Controller;
 
-use http\Client\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-
-class SecurityController extends AbstractController{
-    private $urlGenerator;
-
-
+class SecurityController extends AbstractController
+{
     /**
      * @Route("/login", name="app_login")
-     * @param AuthenticationUtils $authenticationUtils
-     * return Response
-     * @return Response
      */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
 
-public function login(AuthenticationUtils $authenticationUtils): Response{
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
 
-    $error = $authenticationUtils->getLastAuthenticationError();
-
-    $lastUsername = $authenticationUtils->getLastUsername();
-
-    return $this->render('home/home.html.twig', ['last_username'=>$lastUsername, 'error'=>$error]);
-
-
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     /**
-     *@Route ("/logout", name="app_logout")
+     * @Route("/logout", name="app_logout")
      */
     public function logout()
-{
-    return $this->RedirectToRoute("home_index");
-}
-
-
-public function getPassword($credentials): ?string {
-        return $credentials['password'];
-}
-
-public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey){
-        if($targetPath = $this->getTargetPath($request->getSession(), $providerKey)){
-            return new RedirectResponse($targetPath);
-        }
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-}
-
-protected function getLoginUrl(){
-        return $this->urlGenerator->generate('security_login');
-}
-
-    private function getTargetPath($getSession, $providerKey)
     {
-    }
+       return $this->redirectToRoute('home_index');
 
+        //throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
 }
